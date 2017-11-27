124f034f6f4d6dd108a87123827abd3510145d2c
Dmitry Grigorenko <DS.Grigorenko13@gmail.com>
2017-11-27 23:06:35 +0300

    Parsing vk posts

--- vkFeedParser.py
+++ vkFeedParser.py
@@ -1,12 +1,38 @@
 from pprint import pprint
+
+import vk_api
+
 from config import vkToken
-import vk
+
+
+def get_updates():
+    session = vk_api.VkApi(token=vkToken)
+    api = session.get_api()
+
+    response = api.newsfeed.search(q='#python', count=200)
+    posts = response['items']
+    while 'next_from' in response:
+        response = api.newsfeed.search(q='#python', count=200, start_from=response['next_from'])
+        posts += response['items']
+
+    return posts
+
+
+def build_list(raw_list):
+    liked = 0
+    for post in raw_list:
+        if post['likes']['count'] > 1000:
+            liked += 1
+    print(liked)
 
 
 def get_post():
     return 'рандомный пост мать его'
 
 
-session = vk.Session(access_token=vkToken)
-api = vk.API(session)
-pprint(api.users.get(user_ids=1))
+def main():
+    build_list((get_updates()))
+
+
+if __name__ == '__main__':
+    main()
